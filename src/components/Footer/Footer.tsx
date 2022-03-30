f2fa6430a6e46aa40381b5da810e7a7197d112b8
Imoh Henry <henryimoh2@gmail.com>
2022-03-30 22:27:39 +0100

    Add files via upload

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FooterContainer } from './styled';
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <p>
+        Made with love By <a href="https://imohenry.netlify.app">Henry Imoh</a>
+      </p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
